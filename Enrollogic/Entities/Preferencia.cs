ca5e6f4b19d8d8fec7492cab1eabfa924c62249e
ulacit <ulacit@docente.ulacit.ac.cr>
2019-03-09 08:54:50 -0600

    Creacion proyectos
    Bussiness Logic, Entities, Unit Test

---
+++ Enrollogic/Entities/Preferencia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class Preferencia
+    {
+        int id { set; get; }
+        int estudiante { set; get; }
+        string dia { set; get; }
+        string hora { set; get; }
+        string cede { set; get; }
+    }
+}
---
+++ Enrollogic/Entities/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Usuario
+    {
+        int id { set; get; }
+        string nombre { set; get; }
+        string apellido { set; get; }
+        string correo { set; get; }
+        string telefono { set; get; }
+        int tipo { set; get; }
+        string usuario { set; get; }
+        string contrasenna { set; get; }
+    }
+}
